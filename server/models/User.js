39d80006037f5903a5dd7a7e41880cd8ffe703cc
Tara Hensle <tara@northwestern.edu>
2017-07-05 20:36:18 -0500

    merged new model files from master and bcrypt password

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var bcrypt = require("bcrypt");
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -30,7 +31,45 @@ var UserSchema = new Schema({
   	type: String,
   	required: true
   }
-})
+});
+
+//BCrypt Password Code
+
+// **
+//  * Compare the passed password with the value in the database. A model method.
+//  *
+//  * @param {string} password
+//  * @returns {object} callback
+//  */
+UserSchema.methods.comparePassword = function comparePassword(password, callback) {
+  bcrypt.compare(password, this.password, callback);
+};
+
+
+/**
+ * The pre-save hook method.
+ */
+UserSchema.pre('save', function saveHook(next) {
+  const user = this;
+
+  // proceed further only if the password is modified or the user is new
+  if (!user.isModified('password')) return next();
+
+
+  return bcrypt.genSalt((saltError, salt) => {
+    if (saltError) { return next(saltError); }
+
+    return bcrypt.hash(user.password, salt, (hashError, hash) => {
+      if (hashError) { return next(hashError); }
+
+      // replace a password string with hash value
+      user.password = hash;
+
+      return next();
+    });
+  });
+});
+
 
 var User = mongoose.model("User", UserSchema);
 
