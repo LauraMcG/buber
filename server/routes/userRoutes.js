f41ae8cab2e7d18c2c8878befc8c63b1afc13461
Tara Hensle <tara@northwestern.edu>
2017-07-08 11:34:12 -0500

    added user routes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,85 @@
+var passport = require("passport");
+var express = require("express");
+var router = express.Router();
+
+var User = require("../models/User")
+
+// Route to get all Users
+router.get("/Users", function(req, res) {
+  User.find({})
+    .exec(function(err, doc) {
+
+      if (err) {
+        console.log(err);
+      }
+      else {
+        res.json(doc);
+      }
+  });
+
+});
+
+// Route to get new User profile page
+router.get("/Users/:id", function(req, res) {
+  var id = req.params.id;
+
+  User.findOne({'_id': id})
+    .exec(function(err, doc) {
+
+      if (err) {
+        console.log(err);
+      }
+      else {
+        res.json(doc);
+      }
+    });
+});
+
+// Route to add new User to database
+router.post("/Users", function(req, res) {
+  var newUser = new User(req.body);
+  console.log(newUser);
+
+  newUser.save(function(err, doc) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+// Update User in database
+
+router.put("/Users/:id", function(req, res) {
+  
+      var id = req.params.id;
+
+      User.findOneAndUpdate({"_id": id }, {"$set":req.body})
+      .exec(function(err, doc){
+        if (err) {
+          console.log(err)
+        }
+        else {
+          res.json(doc);
+        }
+      })
+  })
+
+// User Delete Route
+router.delete("/Users/:id", function(req, res) {
+  var id = req.params.id;
+
+  User.find({'_id': id}).remove().exec( 
+  function (err) {
+    if (err) {
+      console.log(err)
+    }
+    else {
+      res.send("Deleted");
+    }
+  });
+});
+
+module.exports = router;
